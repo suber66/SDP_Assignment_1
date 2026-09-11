501881bbf69ed7947bd516414a0f617879d57cf6
suber66 <dias.achinbaj@gmail.com>
2026-09-11 15:45:01 +0500

    add $

--- src/TravelDirector.java
+++ src/TravelDirector.java
@@ -3,6 +3,6 @@ public class TravelDirector {
         builder.reset().setDestination("Dubai").setDuration("2 weeks").setPrice("499$");
     }
     public void makeCheapTravel(TravelBuilder builder) {
-        builder.reset().setDestination("Turkey").setDuration("7 days").setPrice("199");
+        builder.reset().setDestination("Turkey").setDuration("7 days").setPrice("199$");
     }
 }
